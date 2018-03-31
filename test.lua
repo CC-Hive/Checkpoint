09aec4f6d9bb23a4709ccbadc0343c9c7a8b8e0b
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2018-03-31 20:01:56 +0100

    init for ask a pro

---
+++ test.lua
@@ -0,0 +1,50 @@
+local checkpoint = dofile(shell.dir().."/checkpoint.lua") --# Checkpoint can also be loaded with require and os.loadAPI
+
+local testRemove = true --false --# try with this set to false then try with it as true
+
+local args = {...}
+
+
+--# Checkpoint works using callbacks, we define a bunch here with inventive names like t, c and d
+local function t(...)
+  print(table.concat({...}, " ")) --# just test code
+  args = {"test"} --# notice that this doesn't effect anything, although 
+  return checkpoint.reach("second") --# this tells Checkpoint what to run next, checkpoint.reach returns whatever the callback returns
+end 
+
+local function c(...)
+  print(table.concat({...}, " "))
+  
+  print("Queuing terminate event, rerun program for next part of test")
+   --os.queueEvent("terminate")
+  return checkpoint.reach("third")
+end
+
+local function d()
+  print("Terminate?")
+  sleep(0.001) --# catch that terminate if we are on first run, second run won't have a terminate
+  print("I'll take that as no")
+  
+  if testRemove then
+    checkpoint.remove("third") --# removing checkpoints is more of a debug thing to make sure that your program is flowing in the right direction
+    print("removing third checkpoint, prepare for error")
+    return checkpoint.reach("third")
+  end
+  return "return test"
+end
+
+--# here we define our checkpoints under the following format:
+--#   label - this is the name of the checkpoint and what your program will need to use to refer to it
+--#   callback - this is the function which gets called when checkpoint.reach is given the corresponding label
+--#   callback args - these are the values passed to the callback when it gets called
+
+checkpoint.add("start", t, 1, unpack(args))
+
+checkpoint.add("second", c, 2, unpack(args))
+
+checkpoint.add("third", d)
+
+
+local r = checkpoint.run("start") --# identifies if your program needs to continue or starts from given label if it doesn't
+
+print(tostring(r)) --# checkpoint.run returns whatever the last checkpoint callback does
